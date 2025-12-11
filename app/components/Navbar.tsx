454b597c7fb346fe84d382cd1fb158a26813cfe7
SamGombs20 <gombajosh18@gmail.com>
2025-12-11 10:41:22 +0300

    Update:Navbar desktop done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,18 +6,20 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
     const pathname = usePathname()
 
-    const isActive = (path:string)=> pathname ===path ? style.active_link : '';
+    const isActive = (path: string) => pathname === path ? style.active_link : '';
     return (
         <div className={style.navbar_container}>
-            <div className={style.navbar_logo_name}>
-                <Image src="/obesiscan_icon.png"
-                    alt="" width={35} height={35} />
-                <p className={style.app_title}><span>Obesi</span>Scan</p>
-            </div>
-            <div className={style.navbar_links}>
-                <Link href="/" className={isActive("/")}><p>Home</p></Link>
-                <Link href="/blog" className={isActive("/blog")}><p>Blog</p></Link>
-                <Link href="/about" className={isActive("/about")}><p>About</p></Link>
+            <div className={style.container}>
+                <div className={style.navbar_logo_name}>
+                    <Image src="/obesiscan_icon.png"
+                        alt="" width={35} height={35} />
+                    <p className={style.app_title}><span>Obesi</span>Scan</p>
+                </div>
+                <div className={style.navbar_links}>
+                    <Link href="/" className={isActive("/")}><p>Home</p></Link>
+                    <Link href="/blog" className={isActive("/blog")}><p>Blog</p></Link>
+                    <Link href="/about" className={isActive("/about")}><p>About</p></Link>
+                </div>
             </div>
         </div>
     );
