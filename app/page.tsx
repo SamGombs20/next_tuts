679ee206e44215ad7af1b5dd6088089ef5d8f36f
SamGombs20 <gombajosh18@gmail.com>
2025-12-11 12:13:01 +0300

    Update:Intro section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import Link from "next/link";
-
+'use client'
+import style from "./styles/home.module.css"
 export default function Main (){
+  const onButtonClick =()=>{
+    alert("Button clicked")
+  }
   return(
     <div>
-      <h3>This is the Home page</h3>
-      <Link href="/about">About page</Link>
+      <div className={style.intro_container}>
+        <p className={style.intro_text}>Obesi<span>Scan</span> - start your <em><strong>journey</strong></em> to better <em><strong>health</strong></em> today</p>
+        <div className={style.check_container}>
+          <p className={style.check_title}>Quickly check your obesity level</p>
+          <p>We collect anonymous data of your personal, physical, diet and lifestyle information.</p>
+          <button onClick={onButtonClick} className={style.check_btn}>Check now!</button>
+        </div>
+      </div>
     </div>
   )
 }
