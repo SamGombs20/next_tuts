27e88959458780c7ffdd24de788d0e2b86408699
SamGombs20 <gombajosh18@gmail.com>
2025-12-12 13:23:43 +0300

    Update:Footer done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import Navbar from "./components/Navbar";
 import "./globals.css"
+import Footer from "./components/Footer";
 
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
