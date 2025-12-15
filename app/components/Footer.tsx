d93f728584bb4e59f26cc69c57eeea23329a90c4
SamGombs20 <gombajosh18@gmail.com>
2025-12-15 10:45:09 +0300

    Update:Footer development

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
         <div className={style.footer_container}>
             <div className={style.footer}>
                 <div className={style.footer_logo_name}>
-                    <Image src="/obesiscan_icon.png"
+                    <Image src="/obesiscan_white.webp"
                         alt="" width={35} height={35} />
                     <p className={style.app_title}><span>Obesi</span>Scan</p>
                 </div>
                 <div className={style.nav_links}>
                     <p className={style.title}>Quick links</p>
                     <div className={style.footer_links}>
-                        <Link href="/"><p>Home</p></Link>
+                        <Link href="/"><p>Quick Check</p></Link>
                         <Link href="/blog"><p>Blog</p></Link>
                         <Link href="/about"><p>About</p></Link>
                     </div>
