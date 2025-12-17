40abe4efa813cde1c3701ac59e837064363a4917
SamGombs20 <gombajosh18@gmail.com>
2025-12-17 15:33:40 +0300

    Update:State management of user inputs

--- app/components/DailyActivity1Modal.tsx
+++ app/components/DailyActivity1Modal.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { FormControl, FormControlLabel, FormLabel,  Radio, RadioGroup } from "@mui/material";
+import { FormControl, FormControlLabel, FormHelperText, FormLabel,  Radio, RadioGroup } from "@mui/material";
 import { radioGroupStyles} from "../styles/MUICustom";
 import style from "../styles/detail_modal.module.css"
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 
 interface Props{
     data:DailyActivity1
@@ -11,7 +11,42 @@ interface Props{
     onSave:()=>void
 }
 export const DailyActivity1:FC<Props> = ({data, setData, onSave}) => {
-    
+    const [errors, setErrors] = useState<DailyActivity1>({
+        waterIntake:'',
+        calorieMonitoring:'',
+        smoke:'',
+        alcohol:''
+    })
+    const handleChange =(event:ChangeEvent<HTMLInputElement>)=>{
+        const {name, value} = event.target
+
+        setData({
+            ...data,
+            [name]:value
+        })
+        setErrors({
+            ...errors,
+            [name]:''
+        })
+    }
+    const validateAndSave =()=>{
+        const newErrors:DailyActivity1 ={
+            waterIntake:'',
+            calorieMonitoring:'',
+            smoke:'',
+            alcohol:''
+        }
+        if(!data.waterIntake) newErrors.waterIntake ="Fill out this field"
+        if(!data.calorieMonitoring) newErrors.calorieMonitoring = "Fill out this field"
+        if(!data.smoke) newErrors.smoke = "Fill out this field"
+        if(!data.alcohol) newErrors.alcohol = "Fill out this field"
+
+        setErrors(newErrors)
+
+        if(Object.values(newErrors).every(value=>value.length===0)){
+            onSave()
+        }
+    }
     return (
         <div className={style.details_form_container}>
             <p className={`${style.form_title} gradient-text`}>
@@ -19,23 +54,25 @@ export const DailyActivity1:FC<Props> = ({data, setData, onSave}) => {
             </p>
             <div className={style.details_form}>
                 <div className={style.radio_container}>
-                    <FormControl sx={radioGroupStyles}>
+                    <FormControl sx={radioGroupStyles} error={!!errors.waterIntake}>
                         <FormLabel id="water">How much water do you drink daily?</FormLabel>
-                        <RadioGroup aria-labelledby="water" name="water-group">
+                        <RadioGroup aria-labelledby="water" value={data.waterIntake} onChange={handleChange} name="waterIntake">
                             <FormControlLabel value="1" control={<Radio />} label="less than 3 glasses" />
                             <FormControlLabel value="2" control={<Radio />} label="3-8 glasses" />
                             <FormControlLabel value="3" control={<Radio />} label="more than 8 glasses" />
 
                         </RadioGroup>
+                        {errors.waterIntake && <FormHelperText>{errors.waterIntake}</FormHelperText>}
                     </FormControl>
                 </div>
                 <div className={style.radio_container}>
-                    <FormControl sx={radioGroupStyles}>
+                    <FormControl sx={radioGroupStyles} error={!!errors.calorieMonitoring}>
                         <FormLabel id="calorie">Do you monitor the calories you eat daily?</FormLabel>
-                        <RadioGroup aria-labelledby="calorie" name="calorie-group">
+                        <RadioGroup aria-labelledby="calorie" value={data.calorieMonitoring} onChange={handleChange} name="calorieMonitoring">
                             <FormControlLabel value="yes" control={<Radio />} label="Yes" />
                             <FormControlLabel value="no" control={<Radio />} label="No" />
                         </RadioGroup>
+                        {errors.calorieMonitoring && <FormHelperText>{errors.calorieMonitoring}</FormHelperText>}
                     </FormControl>
                 </div>
                 <div className={style.radio_container}>
@@ -62,8 +99,8 @@ export const DailyActivity1:FC<Props> = ({data, setData, onSave}) => {
                 </div>
             </div>
             <div className={style.form_buttons}>
-                <button className={`${style.close_btn} btn`}>Cancel</button>
-                <button className={`${style.check_btn} btn`}>Save</button>
+                <button className={`${style.close_btn} btn`} onClick={onSave}>Cancel</button>
+                <button className={`${style.check_btn} btn`} onClick={validateAndSave}>Save</button>
             </div>
         </div>
     );
