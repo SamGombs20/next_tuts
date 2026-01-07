61f2875ae86572b769a75a981418ab3a3064ef23
SamGombs20 <gombajosh18@gmail.com>
2026-01-07 10:58:52 +0300

    Update:Final touches

--- app/api/sanityAPI.ts
+++ app/api/sanityAPI.ts
@@ -1,36 +1,35 @@
-const url =
-  "https://40v865zp.api.sanity.io/v2025-03-26/data/query/production" +
-  "?query=*[_type=='blogPost']{" +
-  "_id," +
-  "title," +
-  "'slug':slug.current," +
-  "'body':body[children[].text!='']," +
-  "mainImage{asset->{url}}," +
-  "author->{_id,name,bio}," +
-  "publishedAt" +
-  "}";
-const slugUrl = "https://40v865zp.api.sanity.io/v2025-03-26/data/query/production?query=*[_type == 'blogPost']{'slug':slug.current}"
-const baseUrl = "https://40v865zp.api.sanity.io/v2025-03-26/data/query/production?query="
-export const getSanityData = async()=>{
-    const res = await fetch(url,{
-        method:"GET"
+
+const baseUrl = process.env.SANITY_URL
+export const getSanityData = async () => {
+    const query = "*[_type=='blogPost']{" +
+        "_id," +
+        "title," +
+        "'slug':slug.current," +
+        "'body':body[children[].text!='']," +
+        "mainImage{asset->{url}}," +
+        "author->{_id,name,bio}," +
+        "publishedAt" +
+        "}"
+    const res = await fetch(`${baseUrl}${encodeURIComponent(query)}`, {
+        method: "GET"
     })
-    if(!res.ok){
+    if (!res.ok) {
         throw new Error("Failed to fetch Sanity data")
     }
     return res.json()
 }
 
-export const getSanitySlugs = async()=>{
-    const res = await fetch(slugUrl,{
-        method:"GET"
+export const getSanitySlugs = async () => {
+    const query = `*[_type == 'blogPost']{'slug':slug.current}`
+    const res = await fetch(`${baseUrl}${encodeURIComponent(query)}`, {
+        method: "GET"
     })
-    if(!res.ok){
+    if (!res.ok) {
         throw new Error("Failed to fetch Sanity slugs")
     }
     return res.json()
 }
-export const getBlog = async(slug:string)=>{
+export const getBlog = async (slug: string) => {
     const query = `
     *[_type == "blogPost" && slug.current == $slug][0]{
       _id,
@@ -47,10 +46,10 @@ export const getBlog = async(slug:string)=>{
       }
     }
     `
-    const res = await fetch(`${baseUrl}${encodeURIComponent(query)}&$slug="${encodeURIComponent(slug)}"`,{
-        method:"GET"
+    const res = await fetch(`${baseUrl}${encodeURIComponent(query)}&$slug="${encodeURIComponent(slug)}"`, {
+        method: "GET"
     })
-    if(!res.ok){
+    if (!res.ok) {
         throw new Error("Failed to fetch blog post")
     }
     return res.json()
